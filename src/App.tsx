2c0ebdf92cb2fad027e69520cf1d27877fb8ce75
이정석 <dlwjdtjr3088@naver.com>
2023-10-25 14:22:13 +0900

    배송 조회 서비스

--- src/App.tsx
+++ src/App.tsx
@@ -1,156 +1,194 @@
 import { useEffect, useState } from "react";
 
 interface Company {
-  International : string;
+  International: string;
   Code: string;
   Name: string;
 }
 
 interface ThemeColor {
-  [key: string] : {                           // 객체는 배열로 불러오기 때문에 [key: string] : {} 이라는 문법을 사용  
-    back: string;                             // 이중 객체에서는 index값을 사용하지 못한다.
+  // 중첩 객체에서는 themeColor[작명한 객체 배열 이름]으로 사용한다.
+  // 예, themeColor[defalut].back, themeColor[orange].hover, themeColor[blue].text
+  [key: string]: {
+    back: string;
     hover: string;
     active: string;
     text: string;
     outline: string;
-  }
+  };
 }
 
 interface ButtonType {
-  name : string;
-  theme : string;
-
+  name: string;
+  theme: string;
 }
 
 function App() {
+  const [allCarriers, setAllCarriers] = useState<Company[]>([]);
+  // 가져온 택배 데이터를 전부 복사해서 넣음 (나중에 이 배열이 필터 될 예정)
+  const [carriers, setCarriers] = useState<Company[]>([]);
+  // 필터 된 값을 넣음
+  const [theme, setTheme] = useState<string>("default");
+  // 디자인 테마를 설정함 (중첩 객체를 활용할 예정)
 
- const [carriers, setCarriers] = useState<Company[]>([]); //필터 되는 정보를 넣을 공간 ...국내/국외 택배사 정보
- const [allCarriers, setAllCarriers] = useState<Company[]>([]); // 전체 데이터
- const [theme, setTheme] = useState<string>('default'); // 색상변경을 위한 코드
+  const [tcode, setTcode] = useState<string>("04");
+  const [tinvoice, setTinvoice] = useState<string>("");
+  const [tname, setTname] = useState<string>("CJ대한통운");
+  const [isBtn, setIsBtn] = useState<number | null>(null);
+  const [infoTracking, setInfoTracking] = useState<string>();
 
- 
- const [tcode, setTcode] = useState<string>('04'); // 코드번호
- const [tinvoice, setInvoice] = useState<string>('') // 실제 운송장 번호
- const [tname, setTname] = useState<string>('CJ대한통운') // 실제 택배사 이름
+  const themeColor: ThemeColor = {
+    default: {
+      back: "bg-indigo-500",
+      hover: "hover:bg-indigo-300",
+      active: "bg-indigo-400",
+      text: "text-indigo-500",
+      outline: "outline-indigo-300"
+    },
+    orange: {
+      back: "bg-orange-500",
+      hover: "hover:bg-orange-300",
+      active: "bg-orange-400",
+      text: "text-orange-500",
+      outline: "outline-orange-300"
+    },
+    blue: {
+      back: "bg-blue-500",
+      hover: "hover:bg-blue-300",
+      active: "bg-blue-400",
+      text: "text-blue-500",
+      outline: "outline-blue-300"
+    },
+  };
 
-const [isBtn, setIsBtn] = useState<number | null>(null);
+  const buttons: ButtonType[] = [
+    { name: "기본", theme: "default" },
+    { name: "오렌지", theme: "orange" },
+    { name: "블루", theme: "blue" },
+  ];
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `http://info.sweettracker.co.kr/api/v1/companylist?t_key=${process.env.REACT_APP_API_KEY}`
+        );
+        const data = await res.json();
+        setAllCarriers(data.Company);
+        setCarriers(data.Company);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
 
+  const selectCode = (BtnNumber: number, code: string, name: string) => {
+    setIsBtn(BtnNumber);
+    setTcode(code);
+    setTname(name);
+    const isInternational = BtnNumber === 2 ? "true" : "false";
+    const filterCarriers = allCarriers.filter(
+      (e) => e.International === isInternational
+    );
+    setCarriers(filterCarriers);
+  };
 
-const themeColor :ThemeColor = {
-  "default":{                         //theme 기본 값
-    "back" : "bg-indigo-500",         //  배경색 색상 지정
-    "hover": "hover:bg-indigo-500",   // hover시 변경될 색상
-    "active": "bg-indigo-400" ,       // active시 색상 변경
-    "text" : "text-indigo-500",        // 글자색 변경
-    "outline": "outline-indigo-300"
-  },
-  "orange":{
-    "back" : "bg-orange-500",
-    "hover": "hover:bg-orange-500",
-    "active": "bg-orange-400",
-    "text" : "text-orange-500",
-    "outline": "outline-orange-300"
-  },
-  "blue":{
-    "back" : "bg-blue-500",
-    "hover": "hover:bg-blue-500",
-    "active": "bg-blue-400",
-    "text" : "text-blue-500",
-    "outline": "outline-blue-300"
+  const blindNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    e.target.value = e.target.value.replace(/[^0-9]/g,``);
+    setTinvoice(value);
   }
-}
 
-const buttons :ButtonType[] = [
-  {name: "기본", theme: "default"},
-  {name: "오렌지", theme: "orange"},
-  {name: "블루", theme: "blue"}
-]
+  const PostSubmit = async () => {
+    // fetch 문을 사용할 것 같으면 아래 코드가 필요, 아닌 경우 searchParams 쓴 코드 쓰면 됨.
+    // const url = new URL(`http://info.sweettracker.co.kr/api/v1/trackingInfo?t_code=${tcode}&t_invoice=${tinvoice}&t_key=${process.env.REACT_APP_API_KEY}`)
 
-useEffect(()=>{
-  const fetchData = async () =>{
+    // const url = new URL("http://info.sweettracker.co.kr/api/v1/trackingInfo");
+    // url.searchParams.append("t_code",tcode);
+    // url.searchParams.append("t_invoice", tinvoice);
+    // url.searchParams.append("t_key", `${process.env.REACT_APP_API_KEY}`)
+
     try{
-      const res = await fetch(`http://info.sweettracker.co.kr/api/v1/companylist?t_key=${process.env.React_APP_API_KEY}`)
-      
+      const res = await fetch(`http://info.sweettracker.co.kr/api/v1/trackingInfo?t_code=${tcode}&t_invoice=${tinvoice}&t_key=${process.env.REACT_APP_API_KEY}`)
       const data = await res.json();
-      setCarriers(data.Company)
-      setAllCarriers(data.Company)
 
-      console.log(carriers)
-      
+      console.log(data)
     }catch(error){
-      console.log(error);
+      console.log(error)
     }
   }
-  fetchData();
-},[])
-
-
-const selectCode =(BtnNumber: number, code: string, name: string)=>{
-  setIsBtn(BtnNumber);         // 국내가 false 1
-  setTcode(code);              // 국외 ture 2
-  setTname(name);
-
-  const isInternational = BtnNumber === 2 ? 'true' : 'false'   //스트링 형태
-  
-  const filterCarriers = allCarriers.filter(e=> e.International === isInternational);
-  setCarriers(filterCarriers)
-}
-
 
   return (
     <>
-     <div className= {`${themeColor[theme].back} p-5 text-black text-sm md:text-xl xl:text-2xl flex justify-between`}>
-        <h3 className="font-extrabold">국내.외 택배조회 시스템</h3>
+      <div
+        className={`${themeColor[theme].back} p-5 text-black text-sm md:text-xl xl:text-2xl flex justify-between`}
+      >
+        <h3 className="font-extrabold text-white">국내.외 택배조회 시스템</h3>
         <div>
-          <span>테마 :</span>
-          {
-            buttons.map((e,i)=>{
-              return(
-                <button key={i} className="mx-1 md:mx-2 xl:mx-3 " onClick={()=> setTheme(e.theme)
-                }>{e.name}</button>
-              )
-            })
-          }
+          <span className="text-white">테마 : </span>
+          {buttons.map((e, i) => {
+            return (
+              <button
+                onClick={() => {
+                  setTheme(e.theme);
+                }}
+                className="mx-1 md:mx-2 xl:mx-3 text-white"
+                key={i}
+              >
+                {e.name}
+              </button>
+            );
+          })}
         </div>
-     </div>
-     <div className="w-4/5 md:w-3/5 xl:4/12 mx-auto my-40 flex rounded items-center pt-2 flex-wrap">
-    <div className="border-b basis-full py-2 px-2 flex justify-center items-center text-sm">
-      <span className="basis-[30%] text-center mr-5">국내 / 국외 선택</span>
-      <button className={`text-sm border p-1 px-5 rounded hover:text-white mr-4 ${isBtn === 1 ? 'text-white' : 'text-black'} ${themeColor[theme].hover} ${isBtn === 1 && themeColor[theme].active} : ''`} onClick={()=> selectCode(1, '04', 'CJ대한통운')}>국내</button>
-      <button className={`text-sm border p-1 px-5 rounded hover:text-white mr-4 ${isBtn === 2 ? 'text-white' : 'text-black'} ${themeColor[theme].hover} ${isBtn === 2 && themeColor[theme].active} : ''`} onClick={()=> selectCode(2, '12', 'EMS')}>국외</button>
-    </div>  
-    
-      <select value={tcode} onChange={(event)=>{     
-        setTcode(event.target.value);
-    }}>
-        {
-          carriers.map((e,i)=>{
-            return(
-              
-              <option key={i} value={e.Code} className="" 
-                
-              >{e.Name}</option>
-            )
-          })
-        }
-      </select>
-      <div className="basis-full py-4 border-b text-center">
-        <input type="text" className="w-[80%] border px-5 py-2 rounded-md outline-indigo-300 " />
       </div>
-    </div>
+      {tinvoice}
+      <div className="w-4/5 md:w-3/5 xl:w-4/12 mx-auto my-40 flex rounded items-center pt-2 flex-wrap">
+        <div className="border-b basis-full py-2 px-2 flex justify-center items-center text-sm">
+          <span className="basis-[30%] text-center mr-5">국내 / 국외 선택</span>
+          <button
+            onClick={() => selectCode(1, "04", "CJ대한통운")}
+            className={`text-sm border p-1 px-5 rounded hover:text-white mr-4 ${
+              isBtn === 1 ? "text-white" : "text-black"
+            } ${themeColor[theme].hover} ${
+              isBtn === 1 ? themeColor[theme].active : ""
+            }`}
+          >
+            국내
+          </button>
+          <button
+            onClick={() => selectCode(2, "12", "EMS")}
+            className={`text-sm border p-1 px-5 rounded hover:text-white ${
+              isBtn === 2 ? "text-white" : "text-black"
+            } ${themeColor[theme].hover} ${
+              isBtn === 2 ? themeColor[theme].active : ""
+            }`}
+          >
+            국외
+          </button>
+        </div>
+        {/* {tcode}{tname} */}
+        <div className="basis-full">
+        <select className="w-full border mt-4 py-2 px-4 rounded-md" onChange={(e) => setTcode(e.target.value)} value={tcode}>
+          {carriers &&
+            carriers.map((e, i) => {
+              return (
+                <option key={i} value={e.Code}>
+                  {e.Name}
+                </option>
+              );
+            })}
+        </select>
+        </div>
+        <div className="basis-full mt-4 py-4 border-b text-center">
+          <input onInput={blindNumber} type="text" placeholder="운송장 번호를 입력해주세요." className={`w-[100%] border px-5 py-2 rounded-md ${themeColor[theme].outline}`}/>
+        </div>
+        <div className="basis-full border-b py-4 text-center">
+          <button onClick={PostSubmit} className={`${themeColor[theme].back} text-white px-5 py-2 rounded-md w-full`}>조회하기</button>
+        </div>
+      </div>
     </>
   );
 }
 
 export default App;
-
-
-/*
-react에서 typescript 적용방법
-
-1. $ npx create-react-app parcel --template typescript
-
-2. cd parcel
-
-*/ 
